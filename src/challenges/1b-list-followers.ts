7b85921932684a2acba603a969e165f1a70d566a
syndercombecreswell <syndercombecreswell@gmail.com>
2022-10-30 21:18:47 +0000

    fixed 1b

--- src/challenges/1b-list-followers.ts
+++ src/challenges/1b-list-followers.ts
@@ -1,7 +1,9 @@
 function listFollowers(followerNames: string[]): string {
   // complete function
   // use array destructuring, for practice!
-  return "fails test right now";
+
+  const [a, b, ...rest] = followerNames
+  return `Followed by ${a}, ${b} and others`;
 }
 
 export default listFollowers;
